c4d94e6005e7b695959f7077541b696539d00156
nurycaroline <nurycaroline@gmail.com>
2019-10-30 14:51:06 -0300

    Fonts no tema

--- src/layouts/themeStyle.js
+++ src/layouts/themeStyle.js
@@ -15,17 +15,21 @@ const theme = {
     blueLight: '#77D0DD',
     yellowLight: '#FFDBA8'
   },
-  mq: {
-    'mobile-small': '320px',
-    'mobile-medium': '375px',
-    'mobile-large': '414px',
-    'mobile': '639px',
-    'tablet': '768px',
-    'desktop': '1024px',
-    'desktop-small': '1280px',
-    'desktop-medium': '1440px',
-    'desktop-big': '1680px',
-    'desktop-large': '1920px'
+  fonts: {
+    Bitter: {
+      italic: '400i',
+      regular: 400,
+      bold: 700
+    },
+    Raleway: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 800,
+      black: 900
+    }
   },
 
   /* Mixins */
