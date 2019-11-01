f42f6b8518ef310c15d3834e2b0d9dd6a6fdf9eb
nurycaroline <nurycaroline@gmail.com>
2019-10-31 21:16:01 -0300

    Remove Modal Planta; Adjusts modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -4,51 +4,22 @@ import { Container, ButtonClose, Header } from './styles';
 import IClose from 'assets/icons/close-white.svg';
 import ICloseGreen from 'assets/icons/close-green.svg';
 
-export default function Modal({
-  open = true,
-  afterOpenModal = () => {},
+export default function Modal ({
   closeModal = () => {},
-  label = 'Example Modal',
   children,
-  stylesC = {},
   className,
-  styleCButtonClose = {},
-  iconButtonWhite = true,
-  header = ''
+  category,
+  local
 }) {
-  const customStyles = {
-    overlay: {
-      top: window.innerWidth > 768 ? 0 : '67px',
-      zIndex: 10,
-      ...stylesC.overlay
-    },
-    container: {
-      ...stylesC.container
-    }
-  };
-
   return (
-    <Container
-      className={className}
-      isOpen={open}
-      onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      contentLabel={label}
-      style={customStyles}
-    >
-      {header && <Header category={header} />}
+    <Container className={className}>
+      <Header category={category} local={local} />
       <ButtonClose
-        iconButtonWhite={iconButtonWhite}
-        style={styleCButtonClose}
         onClick={closeModal}
       >
         <span>Fechar</span>
-        <img
-          src={
-            window.innerWidth > 768 && iconButtonWhite ? IClose : ICloseGreen
-          }
-          alt="Fechar"
-        />
+        <img mq="mobile" src={ICloseGreen} alt="Fechar" />
+        <img mq="dektop" src={IClose} alt="Fechar" />
       </ButtonClose>
 
       {children}
