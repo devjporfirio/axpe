50c5cc9e414bee6da5bf7e55ac2b08941a611d62
Gerson Lima <glima@futurebrand.com>
2019-11-07 14:38:18 -0300

    Loading - New layout

--- src/components/Loading/styles.js
+++ src/components/Loading/styles.js
@@ -30,3 +30,43 @@ export const Container = styled.div`
 export const ContainerSearch = css`
   background: rgba(63, 90, 94, 0.8);
 `;
+
+export const Loader = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  position: relative;
+  margin: 0 auto;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: -8px;
+    left: -8px;
+    width: 100%;
+    height: 100%;
+    border: 8px solid transparent;
+    border-top-color: ${({ theme }) => theme.colors.orange};
+    border-radius: 100%;
+  }
+
+  &:before {
+    z-index: 100;
+    animation: spin 1s infinite;
+  }
+
+  &:after {
+    border: 8px solid ${({ theme }) => theme.colors.white};
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
