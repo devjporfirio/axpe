3c553388c25621f10bcfeacfde3c9e8b64f46c03
nurycaroline <nurycaroline@gmail.com>
2019-11-09 15:21:48 -0300

    Route Work

---
+++ pages/trabalhe-conosco.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from 'pages/Work/styles';
+
+function Work() {
+  return (
+    <Container>
+      <h1>Trabalhe Conosco</h1>
+    </Container>
+  );
+}
+
+export default Work;
