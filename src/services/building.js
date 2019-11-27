d7f61281915096ed73fdfe350ca134eb556b906c
nurycaroline <nurycaroline@gmail.com>
2019-11-27 17:02:03 -0300

    Adjust property

--- src/services/building.js
+++ src/services/building.js
@@ -27,7 +27,7 @@ export default {
     )
       .then(response => response.json())
       .then(data => data);
-    return result && result.length > 0 ? result.results[0] : [];
+    return result && result.results.length > 0 ? result.results[0] : [];
   },
   async getDirections(northeast, southwest) {
     const apiKey = process.env.config.keyMap;
@@ -36,7 +36,7 @@ export default {
     )
       .then(response => response.json())
       .then(data => data);
-    return result && result.length > 0 ? result.results[0] : [];
+    return result && result.results.length > 0 ? result.results[0] : [];
   },
   async getSimilar(property, limit) {
     const params = {
