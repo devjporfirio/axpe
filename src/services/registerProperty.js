15f7324e1276f71a35e000824d02d8bd48dff208
nurycaroline <nurycaroline@gmail.com>
2019-12-03 08:46:51 -0300

    Request Login to register property

--- src/services/registerProperty.js
+++ src/services/registerProperty.js
@@ -1,5 +1,9 @@
+import User from './user';
+
 export default {
   async postProperty(values) {
+    const resToken = await User.postLogin();
+
     const response = await fetch(
       `${process.env.config.apiUrl}/auth/register_your_building`,
       {
@@ -7,8 +11,7 @@ export default {
         body: JSON.stringify(values),
         headers: {
           'Content-Type': 'application/json',
-          Authorization:
-            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9mdXR1cmVicmFuZGZ0cC5jb20uYnJcL3Byb2pldG9zXC9heHBlXC9wYWluZWxcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE1NzUzMTQwMzYsImV4cCI6MTU3NTMxNzYzNiwibmJmIjoxNTc1MzE0MDM2LCJqdGkiOiI0bnR4ODNTajJQMW84YlEyIiwic3ViIjo3LCJwcnYiOiI4N2UwYWYxZWY5ZmQxNTgxMmZkZWM5NzE1M2ExNGUwYjA0NzU0NmFhIn0.yrzxRTyfeuXsXRq4UjDQvArWrmM2ovaSEm_uGp25IaY'
+          Authorization: `Bearer ${resToken.access_token}`
         }
       }
     ).then(response => response.json());
