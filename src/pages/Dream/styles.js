15ca574157fb15aa27f3d220310bf3bb6fb90fae
Felipe Bernardes <bernardes.flp@gmail.com>
2019-12-10 01:55:03 -0300

    Fix Tarefa #123 - SO QUERO SONHAR - Home Desktop - ajustes

--- src/pages/Dream/styles.js
+++ src/pages/Dream/styles.js
@@ -12,11 +12,11 @@ export const Header = styled.header`
   
   ${media.greaterThan('medium')`
       text-align: left;
-      padding: 7vmin 5vmin;
+      padding: 7vmin 5vmin 5vmin 5vmin;
   `}
 
   ${media.greaterThan('large')`
-    padding: 70px 130px 70px 130px;
+    padding: 70px 130px 0 130px;
   `}
 
   h1 {
@@ -42,7 +42,6 @@ export const Header = styled.header`
   p {
       font-family: 'Raleway';
       font-size: .9rem;
-      text-style: sans-serif; //TODO set font family
      
       ${media.greaterThan('large')`
         font-size: 1.1rem;
@@ -72,13 +71,15 @@ export const List = styled.ul`
     min-width: 80vw;
     min-height: 30vh;
     max-width: 90%;
-    margin: .6rem auto;
+    margin-bottom: .6rem;
+    margin-top: .6rem;
     background-image: url('https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
     background-size: cover;
     background-position: center;
     border-radius: 4px;
     text-align: center;
     box-shadow: inset 0 -50px 80px rgba(0,0,0,.5);
+    box-sizing: border-box;
 
     ${media.greaterThan('medium')`
       min-width: 60vw;
@@ -86,12 +87,13 @@ export const List = styled.ul`
     `}
 
     ${media.greaterThan('large')`
-      min-width: 21vw;
+      min-width: 23.5vw;
       min-height: 16vw;
 
       &:hover {
         h2 {
-            margin-top: 45%;
+            margin-top: 40%;
+            top: 1rem;
         }
 
         a:before {
@@ -130,30 +132,33 @@ export const List = styled.ul`
 
   h2 {
     color: white;
-    font-size: 1.1rem;
+    font-size: 1.3rem;
     font-weight: bold;
     margin-top: 35%;
     margin-bottom: 0.4rem;
     transition: all .2s linear;
     font-family: 'Bitter';
     font-weight: ${({ theme }) => theme.fontsWeight.bold};
+    position: relative;
 
     ${media.greaterThan('medium')`
       margin-top: 20%;
     `}
     
     ${media.greaterThan('large')`
-      margin-top: 50%;
+      margin-top: 45%;
+      top: 2rem;
     `}
   }
 
   p {
      color: white;
-     font-size: 0.9rem;
+     font-size: 1.1rem;
      width: 70%;
      transition: all .2s linear;
      font-family: 'Bitter';
      font-weight: ${({ theme }) => theme.fontsWeight.medium};
+     position: relative;
 
      ${media.greaterThan('medium')`
       width: 45%;
@@ -162,6 +167,7 @@ export const List = styled.ul`
      ${media.greaterThan('large')`
         opacity: 0;
         width: 60%;
+        top: 1.3rem;
     `}
   }
 `;
