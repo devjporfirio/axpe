b4b4d6c21bbf0c3ba908099ee0363427be8cd6ab
Felipe Bernardes <bernardes.flp@gmail.com>
2019-12-13 02:12:22 -0300

    Itera so quero sonhar detalhes

--- pages/so-quero-sonhar-detalhe.jsx
+++ pages/so-quero-sonhar-detalhe.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
-import { Container } from 'pages/Dream/Detail/styles';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Container, Header, List } from 'pages/Dream/Detail/styles';
+import Api from 'services';
+import Building from 'components/Building';
+
+// styles
+import {
+  HeaderCombo,
+  Wrapper,
+  ButtonBack,
+  Buildings,
+  BuildingsNotFound,
+  BuildingsLoadMore
+} from 'pages/Search/styles'
 
 function DreamDetail() {
+  const [ buildings, setBuildings, total ] = useState(null);
+
+  const setNewData = useCallback((newData, first) => {
+    const newBuildings = buildings && buildings.length && !first ? [ ...buildings, ...newData ] : [ ...newData ];
+    setBuildings(newBuildings);
+    //setIsLoading(false);
+  }, [ buildings ]);
+
+  useEffect(() => {
+    const getData = async () => {
+
+      const params = {source: 'praia', finality: 'aluguel', limit: 5};
+      const response = await Api.Search.getBuildings(params);
+      console.log('res', response);
+      
+      
+
+      setNewData(response.data, true);
+    }
+
+    getData();
+
+  }, [ total, setNewData ]);
+
   return (
     <Container>
-        <h1>Só Quero <span>Sonhar</span> Detalhe</h1>
+      <Header>
+        <h1>Só quero Sonhar<span>Descolados</span></h1>
+        <p>Lorem ipsum sit dolor sit amet, consecteur elit. Sed nec eros. Lorem ipsum sit dolor sit amet, consecteur elit. Sed nec eros</p>
+      </Header>
+
+      <List>
+        <h2>Confira nossa seleção com as casas mais <span>descoladas</span></h2>
+        
+        <Buildings>
+          {buildings && buildings.length > 0 ? buildings.map((building, buildingIndex) => (
+              <Building item={building} key={`building-searchitem-${building.reference}-${buildingIndex}`} />
+            )) : (
+            <BuildingsNotFound>
+              <h6>Não encontramos o imóveis na categoria que você procura <span>:(</span></h6>
+              <p>Tente fazer uma <a href="/search">busca!</a></p>
+            </BuildingsNotFound>
+          )} 
+        </Buildings>
+      </List>
+
+      <footer>
+        <h2>Sonhe também com:</h2>
+        <ul>
+          <li></li>
+        </ul>
+      </footer>
     </Container>
   )
 }
--- src/pages/Dream/Detail/styles.js
+++ src/pages/Dream/Detail/styles.js
@@ -11,38 +11,57 @@ export const Header = styled.header`
   text-align: center;
   
   ${media.greaterThan('medium')`
-      text-align: left;
-      padding: 7vmin 5vmin;
+    padding: 7vmin 5vmin 0 5vmin;
   `}
 
   ${media.greaterThan('large')`
-    padding: 70px 130px 70px 130px;
+    padding: 70px 130px 0 130px;
   `}
 
   h1 {
-    font-size: 7.7vmin;
+    color: ${({ theme }) => theme.colors.orange};
+    font-size: 1.3rem;
     letter-spacing: 0.2vmin;
-    font-family: 'Bitter';
+    font-family: 'Raleway';
     font-weight: ${({ theme }) => theme.fontsWeight.bold};
-    margin-bottom: 1.5rem;
-
-    ${media.greaterThan('medium')`
-      font-size: 6vmin;
-    `}
-
-    ${media.greaterThan('large')`
-      font-size: 7.7vmin;
-    `}
+    margin-bottom: 4rem;
+    position: relative;
   
     span {
-        color: ${({ theme }) => theme.colors.orange};
+        color: ${({ theme }) => theme.colors.greenDark};
+        font-weight: ${({ theme }) => theme.fontsWeight.medium};
+        margin-top: 1rem;
+        display: block;
+        font-size: 7.7vmin;
+        font-family: 'Bitter';
+
+        ${media.greaterThan('medium')`
+          font-size: 6vmin;
+        `}
+
+        ${media.greaterThan('large')`
+          font-size: 7.7vmin;
+        `}
+    }
+
+    &:after {
+      content: '';
+      width: 200px;
+      height: 4px;
+      background-color: ${({ theme }) => theme.colors.orange};
+      position: absolute;
+      top: 115%;
+      left: 50%;
+      transform: translateX(-50%);
     }
   }
 
   p {
       font-family: 'Raleway';
-      font-size: .9rem;
+      font-size: 1.1rem;
       text-style: sans-serif; //TODO set font family
+      max-width: 60%;
+      margin: 0 auto;
      
       ${media.greaterThan('large')`
         font-size: 1.1rem;
@@ -50,10 +69,7 @@ export const Header = styled.header`
   }
 `;
 
-export const List = styled.ul`
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
+export const List = styled.main`
   padding: 0 5vmin 10vmin 5vmin;
 
   ${media.greaterThan('medium')`
@@ -63,105 +79,20 @@ export const List = styled.ul`
   ${media.greaterThan('large')`
     padding: 70px 130px 70px 130px;
     text-align: left;
-    flex-flow: row wrap;
-    justify-content: space-between;
   `}
 
-  li {
-    overflow: hidden;
-    min-width: 80vw;
-    min-height: 30vh;
-    max-width: 90%;
-    margin: .6rem auto;
-    background-image: url('https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
-    background-size: cover;
-    background-position: center;
-    border-radius: 4px;
-    text-align: center;
-    box-shadow: inset 0 -50px 80px rgba(0,0,0,.5);
-
-    ${media.greaterThan('medium')`
-      min-width: 60vw;
-      min-height: 25vh;
-    `}
-
-    ${media.greaterThan('large')`
-      min-width: 21vw;
-      min-height: 16vw;
-
-      &:hover {
-        h2 {
-            margin-top: 45%;
-        }
-
-        a:before {
-            opacity: 0.5;
-        }
-
-        p {
-            opacity: 1;
-        }
-      }
-    `}
-  }
-
-  a {
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    position: relative;
-
-    &:before {
-        content: '';
-        opacity: 0;
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        background-color: ${({ theme }) => theme.colors.orange};
-        transition: all .2s linear;
-        z-index: -1;
-    }
-  }
-
   h2 {
-    color: white;
-    font-size: 1.1rem;
-    font-weight: bold;
-    margin-top: 35%;
-    margin-bottom: 0.4rem;
-    transition: all .2s linear;
+    color: ${({ theme }) => theme.colors.greenDark};
+    font-size: 5vmin;
+    max-width: 800px;
+    line-height: 1.2;
+    letter-spacing: 0.2vmin;
     font-family: 'Bitter';
-    font-weight: ${({ theme }) => theme.fontsWeight.bold};
-
-    ${media.greaterThan('medium')`
-      margin-top: 20%;
-    `}
-    
-    ${media.greaterThan('large')`
-      margin-top: 50%;
-    `}
-  }
-
-  p {
-     color: white;
-     font-size: 0.9rem;
-     width: 70%;
-     transition: all .2s linear;
-     font-family: 'Bitter';
-     font-weight: ${({ theme }) => theme.fontsWeight.medium};
-
-     ${media.greaterThan('medium')`
-      width: 45%;
-    `}
-
-     ${media.greaterThan('large')`
-        opacity: 0;
-        width: 60%;
-    `}
+    font-weight: ${({ theme }) => theme.fontsWeight.medium};
+    margin-bottom: 4rem;
+  
+    span {
+        color: ${({ theme }) => theme.colors.orange};
+    }
   }
 `;
