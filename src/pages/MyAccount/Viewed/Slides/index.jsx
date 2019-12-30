71daafa8cd2989da46a85d274fad595cf0a0c10f
nurycaroline <nurycaroline@gmail.com>
2019-12-30 10:53:24 -0300

    ajusted warning about prop key on link and warning about <a>

--- src/pages/MyAccount/Viewed/Slides/index.jsx
+++ src/pages/MyAccount/Viewed/Slides/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Link from 'next/link';
 import SVG from 'react-inlinesvg';
 
@@ -46,32 +46,33 @@ export default function Slides({ items = [], date }) {
       >
         {items.length > 0 &&
           items.map((item, index) => (
-            <Link
-              key={index}
-              href={`/building/${
-                item.building ? item.building.slug : item.slug
-              }`}
-            >
-              <a
+            <Fragment key={index}>
+              <Link
                 href={`/building/${
                   item.building ? item.building.slug : item.slug
                 }`}
               >
-                {item.status === 'inactive' && (
-                  <Inactive>
-                    <SVG src={IEmoji} />
-                    <p>
-                      <strong>Ops!</strong>
-                      <br />
-                      Esse imóvel não está mais disponível
-                    </p>
-                  </Inactive>
-                )}
-                <Image mq="desktop" src={item.imageFeatured.desktop} />
-                <Image mq="mobile" src={item.imageFeatured.mobile} />
-                <Section type="slickSmall" item={item} />
-              </a>
-            </Link>
+                <a
+                  href={`/building/${
+                    item.building ? item.building.slug : item.slug
+                  }`}
+                >
+                  {item.status === 'inactive' && (
+                    <Inactive>
+                      <SVG src={IEmoji} />
+                      <p>
+                        <strong>Ops!</strong>
+                        <br />
+                        Esse imóvel não está mais disponível
+                      </p>
+                    </Inactive>
+                  )}
+                  <Image mq="desktop" src={item.imageFeatured.desktop} />
+                  <Image mq="mobile" src={item.imageFeatured.mobile} />
+                  <Section type="slickSmall" item={item} useButtom={false} />
+                </a>
+              </Link>
+            </Fragment>
           ))}
       </Slide>
     </Panel>
