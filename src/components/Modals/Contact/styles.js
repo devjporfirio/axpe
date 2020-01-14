1d9d407cb14528a6b23f2e3548ac04524709736b
nurycaroline <nurycaroline@gmail.com>
2020-01-14 16:30:40 -0300

    Adjusted to set background-color on label, not on textarea

--- src/components/Modals/Contact/styles.js
+++ src/components/Modals/Contact/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-  textarea {
+  label {
     background-color: ${({ theme }) => theme.colors.grey};
     border-radius: 4px;
   }
