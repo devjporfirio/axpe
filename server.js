e9bcceb7844c5e55e1a6f56eceb1ec81caa6a862
Gerson Lima <gersonthiago.lima@gmail.com>
2020-01-29 21:08:56 +0000

    Check notifications 30 seconds

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ const getNotifications = async (userId) => {
 
 io.on('connection', socket => {
   let timer;
-  const timing = 5000;
+  // const timing = 5000;
+  const timing = 30000;
 
   socket.on('notifications-init', (userId) => {
     if(timer) {
