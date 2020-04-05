86b9a27fb192e17d3fedd78057c52ff1979891a0
Gerson Lima <gersonthiago.lima@gmail.com>
2020-04-05 19:24:47 +0100

    Building URL fix

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,5 +1,3 @@
-import slugify from 'slugify';
-
 export const shuffle = list => {
   return list.sort(() => Math.random() - 0.5);
 };
@@ -28,57 +26,22 @@ export const getParamsFromObject = (params) => {
 
 export const getBuildingUrl = (obj) => {
   let baseSlug = 'imovel';
-  const slug = [ obj.type ];
-  const reference = obj.reference;
-  const neighborhood = obj.address && obj.address.local ? slugify(obj.address.local.toLowerCase()) : null;
-  const city = obj.address && obj.address.region ? slugify(obj.address.region.toLowerCase()) : null;
-  const areaUseful = obj.infos && obj.infos.areaUseful ? `${parseInt(obj.infos.areaUseful)}m` : null;
+  const type = obj.type;
 
   // /imovel/[tipo]-[comprar/alugar]-[bairro]-sp-[m²]-[referencia]
   // /imovel/[tipo]-[comprar/alugar]-[cidade]-[país]-[m²]-[referencia]
   // /lancamentos/[tipo]-[comprar/alugar]-[bairro]-sp-[referencia]
   // /[praia/campo]/[tipo]-[comprar/alugar]-[cidade]-[m²]-[referencia]
 
-  if(obj.infos) {
-    if(obj.infos.type === 'VENDA') {
-      slug.push('comprar');
-    } else if(obj.infos.type === 'ALUGUEL') {
-      slug.push('alugar');
-    } else {
-      slug.push('comprar-alugar');
-    }
-  } else {
-    slug.push('comprar-alugar');
-  }
-
-  if(neighborhood) {
-    slug.push(neighborhood);
-  }
-
-  if(city) {
-    slug.push(city);
-  }
-
   if(obj.source === 'praia') {
     baseSlug = 'praia';
   } else if(obj.source === 'campo') {
     baseSlug = 'campo';
-  }
-
-  if(areaUseful) {
-    slug.push(areaUseful);
-  }
-
-  if(reference) {
-    slug.push(reference);
-  }
-
-  if(slug[0] === 'lancamento') {
-    slug.shift();
+  } else if(type === 'lancamento') {
     baseSlug = 'lancamentos';
   }
 
-  return `/${baseSlug}/${slug.join('-')}`;
+  return `/${baseSlug}/${obj.slug}`;
 }
 
 export const checkPluralSingular = (word, size) => {
