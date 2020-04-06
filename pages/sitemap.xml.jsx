981ae421062040cebd5ddc63e6b7fea52ba98b0e
Gerson Lima <gersonthiago.lima@gmail.com>
2020-04-06 01:32:48 +0100

    Sitemap.xml page

---
+++ pages/sitemap.xml.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Api from 'services';
+
+const renderXml = ({ buildings, landings }) => {
+  const siteUrl = `http://homolog.axpe.com.br`;
+  const pages = [
+    `<url><loc>${siteUrl}/sobre</loc><priority>0.80</priority></url>`,
+    `<url><loc>${siteUrl}/contato</loc><priority>0.80</priority></url>`,
+  ];
+
+  if(landings && landings.length) {
+    landings.forEach(item => {
+      pages.push(`<url><loc>${siteUrl}/${item.slug}</loc><priority>0.60</priority></url>`);
+    })
+  }
+
+  if(buildings['all'] && buildings['all'].length) {
+    buildings['all'].forEach(item => {
+      pages.push(`<url><loc>${siteUrl}/${item.slug}</loc><priority>0.80</priority></url>`);
+    })
+  }
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${siteUrl}</loc>
+      <priority>1.00</priority>
+    </url>
+    ${pages.join('')}
+  </urlset>`;
+};
+
+export default class SiteMapFile extends React.Component {
+  static async getInitialProps({ res }) {
+    const response = await Api.SiteMap.getPage();
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(renderXml(response));
+    res.end();
+  }
+}
