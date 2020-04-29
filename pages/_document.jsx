2ae8158c01b2fa7169325630985c74f3391de192
Gerson Lima <gersonthiago.lima@gmail.com>
2020-04-29 16:16:50 +0100

    Favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -43,30 +43,27 @@ export default class MyDocument extends Document {
           <meta name='application-name' content='Axpe' />
           <link rel='manifest' href='/manifest.json' />
 
-          {/* <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='Axpe' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='theme-color' content='#000000' />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel='shortcut icon' href='/favicon/favicon.ico' />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
 
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
-          <link rel='manifest' href='/manifest.json' />
-          <link rel='shortcut icon' href='/static/icons/favicon.ico' />
+          {/*
 
           <meta name='twitter:card' content='summary' />
           <meta name='twitter:url' content='https://axpe.com.br' />
           <meta name='twitter:title' content='Axpe' />
           <meta name='twitter:description' content='Axpe Description' />
-          <meta name='twitter:image' content='https://axpe.com.br/static/icons/android-chrome-192x192.png' />
+          <meta name='twitter:image' content='https://axpe.com.br/favicon/android-chrome-192x192.png' />
           <meta name='twitter:creator' content='@DavidWShadow' />
           <meta property='og:type' content='website' />
           <meta property='og:title' content='Axpe' />
           <meta property='og:description' content='Axpe Description' />
           <meta property='og:site_name' content='Axpe' />
           <meta property='og:url' content='https://axpe.com.br' />
-          <meta property='og:image' content='https://axpe.com.br/static/icons/apple-touch-icon.png' /> */}
+          <meta property='og:image' content='https://axpe.com.br/favicon/apple-touch-icon.png' /> */}
 
           <HeaderStyles />
         </Head>
