b66ac219085843c6335575ef859f33217ba4beff
Rodolfo Araki <rodolfo@tinpix.com.br>
2020-12-04 17:18:28 -0300

    UTM params on single building Zoho forms

--- public/forms/scripts/main.js
+++ public/forms/scripts/main.js
@@ -130,4 +130,35 @@
       this.submit();
     }
   }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  var cookieParams = getCookie('ax_utm_params');
+  if(cookieParams) {
+    var utmParams = JSON.parse(cookieParams);
+
+    Object.entries(utmParams).forEach(([key, value]) => {
+      console.log(`${key} ${value}`);
+      var $field = $form.querySelector(`input[name="${key}"]`);
+
+      if($field)
+        $field.value = value;
+    });
+
+  }
+  
 })()
