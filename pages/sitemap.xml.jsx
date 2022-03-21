870681c5340b77d661f75af084f841605777dd93
paulamariano <pmariano@futurebrand.com>
2022-03-21 11:24:30 -0300

    Adicionando 2 novas páginas ao mapa do site. Política de privacidade e termos de uso.

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -9,6 +9,8 @@ const renderXml = ({ buildings, landings }) => {
     `<url><loc>${siteUrl}/sobre</loc><priority>0.80</priority></url>`,
     `<url><loc>${siteUrl}/contato</loc><priority>0.80</priority></url>`,
     `<url><loc>${siteUrl}/so-quero-sonhar</loc><priority>0.60</priority></url>`,
+    `<url><loc>${siteUrl}/politica-de-privacidade</loc><priority>0.60</priority></url>`,
+    `<url><loc>${siteUrl}/termos-de-uso</loc><priority>0.60</priority></url>`,
   ];
 
   if(landings && landings.length) {
