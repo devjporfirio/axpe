87b640a7f504903f299dfe99e504870339b7d292
Gerson Lima <gersonthiago.lima@gmail.com>
2023-01-31 17:12:57 +0000

    chore(landing): remove gradient from cover image

--- pages/landing/[slug].jsx
+++ pages/landing/[slug].jsx
@@ -14,7 +14,7 @@ import {
   Container,
   Hero,
   Image,
-  Gradient,
+  // Gradient,
   Title,
   GroupText,
   TextContainer,
@@ -42,77 +42,87 @@ function Landing({ slug, page }) {
     }
   }, []);
 
-  return slug ? (
-    <>
-      <Container>
-        {images.desktop || images.mobile ? (
-          <Hero>
-            <Gradient />
-            <Image mq="desktop" src={images.desktop} />
-            <Image mq="mobile" src={images.mobile} />
-          </Hero>
-        ) : null}
-        <Title>{title}</Title>
-        <hr />
-        <GroupText>
-          <TextContainer transparent={transparent}>
-            <Text ref={textEl}>{text}</Text>
-          </TextContainer>
-          {!transparent && (
-            <SeeMore type="button" onClick={() => setTransparent(true)}>
-              Veja Mais
-            </SeeMore>
-          )}
-        </GroupText>
+  if (!slug) return null;
 
-        {componentes &&
-          componentes.length > 0 &&
-          componentes.map((comp, index) => {
-            switch (comp.type) {
-              case 'imoveis-vertical':
-                return (
-                  <Module key={`landing-component-${index}`}>
-                    <TitleModule>
-                      <strong>{comp.info.title}</strong> {comp.info.subtitle}
-                    </TitleModule>
-                    <TextModule>{comp.info.text}</TextModule>
+  return (
+    <Container>
+      {images.desktop || images.mobile ? (
+        <Hero>
+          {/* <Gradient /> */}
+          <Image mq="desktop" src={images.desktop} />
+          <Image mq="mobile" src={images.mobile} />
+        </Hero>
+      ) : null}
+      <Title>{title}</Title>
+      <hr />
+      <GroupText>
+        <TextContainer transparent={transparent}>
+          <Text ref={textEl}>{text}</Text>
+        </TextContainer>
+        {!transparent && (
+          <SeeMore type="button" onClick={() => setTransparent(true)}>
+            Veja Mais
+          </SeeMore>
+        )}
+      </GroupText>
 
-                    {comp.buildings &&
-                      comp.buildings.length > 0 &&
-                      comp.buildings.map((building) => (
-                        building && building.status === 'active'
-                          ? <BuildingList key={building.reference} item={building} />
-                          : null
-                      ))}
-                  </Module>
-                );
-              case 'imoveis-horizontal':
-                return (
-                  <Module key={`landing-component-${index}`}>
-                    <TitleModule>
-                      <strong>{comp.info.title}</strong> {comp.info.subtitle}
-                    </TitleModule>
-                    <TextModule>{comp.info.text}</TextModule>
-                    <SlideSmall type="slickSmall" items={comp.buildings} />
-                  </Module>
-                );
+      {componentes &&
+        componentes.length > 0 &&
+        componentes.map((comp, index) => {
+          switch (comp.type) {
+            case 'imoveis-vertical':
+              return (
+                <Module key={`landing-component-${index}`}>
+                  <TitleModule>
+                    <strong>{comp.info.title}</strong> {comp.info.subtitle}
+                  </TitleModule>
+                  <TextModule>{comp.info.text}</TextModule>
+
+                  {comp.buildings &&
+                    comp.buildings.length > 0 &&
+                    comp.buildings.map((building) =>
+                      building && building.status === 'active' ? (
+                        <BuildingList
+                          key={building.reference}
+                          item={building}
+                        />
+                      ) : null
+                    )}
+                </Module>
+              );
+            case 'imoveis-horizontal':
+              return (
+                <Module key={`landing-component-${index}`}>
+                  <TitleModule>
+                    <strong>{comp.info.title}</strong> {comp.info.subtitle}
+                  </TitleModule>
+                  <TextModule>{comp.info.text}</TextModule>
+                  <SlideSmall type="slickSmall" items={comp.buildings} />
+                </Module>
+              );
+          }
+        })}
+
+      {Object.keys(imagesBanner).length > 0 &&
+        imagesBanner.desktop &&
+        imagesBanner.mobile && (
+          <Banner
+            href={imagesBanner.link.url}
+            target={
+              imagesBanner.link.external || imagesBanner.link.target !== 'self'
+                ? '_blank'
+                : '_self'
             }
-          })}
+          >
+            <Image mq="desktop" src={imagesBanner.desktop} />
+            <Image mq="mobile" src={imagesBanner.mobile} />
+          </Banner>
+        )}
 
-        {Object.keys(imagesBanner).length > 0 &&
-          imagesBanner.desktop &&
-          imagesBanner.mobile && (
-            <Banner href={imagesBanner.link.url} target={(imagesBanner.link.external || imagesBanner.link.target !== 'self') ? '_blank' : '_self'}>
-              <Image mq="desktop" src={imagesBanner.desktop} />
-              <Image mq="mobile" src={imagesBanner.mobile} />
-            </Banner>
-          )}
-
-        <BlockHighlighted type="notfound" />
-        <Contact />
-      </Container>
-    </>
-  ) : null;
+      <BlockHighlighted type="notfound" />
+      <Contact />
+    </Container>
+  );
 }
 
 Landing.getInitialProps = async ({ query }) => {
@@ -122,7 +132,7 @@ Landing.getInitialProps = async ({ query }) => {
   const pageTitle = `${response.title} ${SeoData.shortTitle}`;
   const pageDesc = `${response.title}. Aqui na Axpe você encontra o imóvel perfeito para suas necessidades!`;
 
-  const pageBanner = (response.images.desktop) ? response.images.desktop : null;
+  const pageBanner = response.images.desktop ? response.images.desktop : null;
 
   return {
     slug,
@@ -130,8 +140,8 @@ Landing.getInitialProps = async ({ query }) => {
     meta: {
       title: pageTitle,
       description: pageDesc,
-      image: pageBanner
-    }
+      image: pageBanner,
+    },
   };
 };
 
