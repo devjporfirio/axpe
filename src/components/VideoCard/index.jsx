fc7c978a1f5ec8ba3f55f8d6b05c41b9e03250e8
João Porfirio <joao.porfirio@wicomm.com.br>
2026-06-03 10:27:49 -0300

    fix: task16793897 - [AXPE] - Ajustes

--- src/components/VideoCard/index.jsx
+++ src/components/VideoCard/index.jsx
@@ -76,7 +76,7 @@ const VideoCard = ({ reference }) => {
             <iframe
               width="100%"
               height="517"
-              src={`https://www.youtube.com/embed/${youtubeId}?controls=0&modestbranding=1&rel=0&fs=0&iv_load_policy=3&disablekb=1`}
+              src={`https://www.youtube.com/embed/${youtubeId}?controls=0&modestbranding=0&rel=0&fs=0&iv_load_policy=3&disablekb=1`}
               title={video?.video_title}
               frameBorder="0"
               allow="encrypted-media"
